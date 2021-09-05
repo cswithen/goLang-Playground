9ec6d4678c42b68c918365c6efbcd73471839ccc
cswithen <cody.swithenbank@gmail.com>
2021-09-05 13:57:44 -0700

    finished go routines section

--- channels/main.go
+++ channels/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -32,8 +33,38 @@ func main() {
 	// this will tell the channel to wait for !a! call and then stop the main routine
 	// fmt.Println(<-c)
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	// this is a for loop that runs for the length of the links slice
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	/*
+		// this is a for loop that will continue to run until the program is finished (infinite loop)
+		for {
+			// this repeats the creation of the go routine
+			go checkLink(<-c, c)
+		}
+	*/
+
+	/*
+		// refactor of the loop above to make it 'easier' to debug
+		// wait for the channel to receive a value and then assign it to the variable l
+		for l := range c {
+			// pauses the current go routine for x number of seconds time.Sleep(x * time.Second)
+			time.Sleep(5 * time.Second)
+			go checkLink(l, c)
+		}
+	*/
+
+	// this defines a function literal[anonymous function] the () at the end calls the function literal
+	for l := range c {
+		// in order to utilize the pass by value nuance in go
+		// pass in l into the 'function literal' as an argument
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+			// and then when calling the function be sure to use l
+		}(l)
 	}
 }
 
@@ -43,9 +74,15 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		// this pushes a message into the channel
+		// c <- "Might be down I think"
+		// this pushes the link variable into the channel
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "Yep its up"
+	// this pushes a message into the channel
+	// c <- "Yep its up"
+	// this pushes the link variable into the channel
+	c <- link
 }
